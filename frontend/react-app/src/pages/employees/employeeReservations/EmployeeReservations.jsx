f9bc573cc9a4d84d320d05e0f9a1a7a9f086e91e
Daniel Mañogil Lasheras <daniel9alc@gmail.com>
2025-03-12 21:52:14 +0100

    Actualización 12-03-25

--- frontend/react-app/src/pages/employees/employeeReservations/EmployeeReservations.jsx
+++ frontend/react-app/src/pages/employees/employeeReservations/EmployeeReservations.jsx
@@ -1,11 +1,14 @@
 import { EmpLoginNav } from "../components/employeeLoginNav/EmployeeLoginNav"
-import { Container } from "react-bootstrap"
-import EmpResrvationsFilter from "./components/employeeReservationsFilter/EmployeeReservationsFilter"
+import { Container } from "react-bootstrap";
+import EmpResrvationsFilter from "./components/employeeReservationsFilter/EmployeeReservationsFilter";
+import EmpReservationsList from "./components/employeeReservationsList/EmployeeReservationsList";
+import "./employeeReservations.css";
  export default function EmpReservations(){
     return(
         <Container fluid className="m-0 p-0">
             <EmpLoginNav/>
-            <EmpResrvationsFilter/>
+            <EmpResrvationsFilter />
+            <EmpReservationsList className="mt-5"/>
         </Container>
         
     )
